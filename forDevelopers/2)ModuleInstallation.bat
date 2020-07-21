a5b0e96a207f028e42ef1e2f995fecf58e94cb5e
Dmitriy Schalimov <dmitriy-shalimov@yandex.ru>
2020-07-21 12:18:01 +0300

    Update 2)ModuleInstallation.bat

--- forDevelopers/2)ModuleInstallation.bat
+++ forDevelopers/2)ModuleInstallation.bat
@@ -1,10 +1,6 @@
-@: Authors of the projects:
-@: 1-MachnevEgor_https://vk.com/machnev_egor
-@: 2-DmitryShalimov_https://vk.com/lookatstat
-@: Contacts in email:
-@: 1-mea90608744@gmail.com
-@: 2-dmitriy-shalimov@yandex.ru
 pip install discord.py
+
+__________________________
 @: Authors of the projects:
 @: 1-MachnevEgor_https://vk.com/machnev_egor
 @: 2-DmitryShalimov_https://vk.com/lookatstat
