6871ff4b75dca4e88315bc86c85bc01ebad9957e
machnev_egor <46050097+machnevegor@users.noreply.github.com>
2020-08-20 12:10:10 +0300

    Add files via upload

---
+++ botCode/configurationFile/BotConfig.py
@@ -0,0 +1,17 @@
+# Authors of the projects:
+# 1-MachnevEgor_https://vk.com/machnev_egor
+# 2-DmitryShalimov_https://vk.com/lookatstat
+# Contacts in email:
+# 1-mea90608744@gmail.com
+# 2-dmitriy-shalimov@yandex.ru
+
+# https://discord.com/api/oauth2/authorize?client_id=734057836652396546&permissions=1345325632&scope=bot
+BotToken = ""  # bot token
+link_to_bot_site = ""  # link to the official site of the bot
+
+# Authors of the projects:
+# 1-MachnevEgor_https://vk.com/machnev_egor
+# 2-DmitryShalimov_https://vk.com/lookatstat
+# Contacts in email:
+# 1-mea90608744@gmail.com
+# 2-dmitriy-shalimov@yandex.ru
